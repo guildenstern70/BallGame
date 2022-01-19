227262e4fcf797f2b1fa888ab5faccd5602699fc
Alessio Saltarin <alessiosaltarin@gmail.com>
2022-01-19 10:58:35 +0100

    Test fix

--- BallGame/test/test_dao.py
+++ BallGame/test/test_dao.py
@@ -5,9 +5,6 @@
 #  This software is distributed under MIT License.
 #  See LICENSE.
 #
-import django
-django.setup()
-
 from django.test import TestCase
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password
